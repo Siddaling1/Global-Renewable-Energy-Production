bda053b8ff5c5f700d6a744061d5e340a695a361
Siddaling <sollasiddaling@gmail.com>
2025-11-01 14:22:10 +0530

    Update global_renewable_energy_production.sql

--- global_renewable_energy_production.sql
+++ global_renewable_energy_production.sql
@@ -2,7 +2,7 @@ CREATE DATABASE global_renewable_energy_production;
 
 USE global_renewable_energy_production;
 
-
+# Questions
 SELECT DISTINCT Country_Name, Continent
 FROM global_renewable_energy_production;
 
